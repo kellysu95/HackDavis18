7713f9c6328aca079b336848614f18c2ffc15a10
r-eter <reter@ucdavis.edu>
2018-01-20 15:28:01 -0800

    Test22222

--- appengine/standard/hello_world/main.py
+++ appengine/standard/hello_world/main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hows it going!')
+        self.response.write('Hows it going! SUS TRACKER IS GONNA TAKE OVER.')
 
 
 app = webapp2.WSGIApplication([
